cce64ee7b45253cab57a627850c4573c82dde1d4
Peter Molfese <pmolfese@gmail.com>
2026-03-19 15:51:59 -0400

    Initial Commit

---
+++ EEGView/EEGViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EEGViewApp.swift
+//  EEGView
+//
+//  Created by Molfese, Peter  [E] on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct EEGViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
